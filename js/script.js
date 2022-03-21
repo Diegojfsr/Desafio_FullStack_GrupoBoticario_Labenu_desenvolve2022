7d103b77165dcaa8591f69c20d80f21e358f8e90
Diego Jefferson <diegojfsr@gmail.com>
2022-03-21 00:44:18 -0300

    Desafio FullStack Labenu

--- js/script.js
+++ js/script.js
@@ -1,87 +1,18 @@
-const fields = document.querySelectorAll("[required]")
+const inputs = document.querySelectorAll(".input");
 
-function ValidateField(field) {
-    // logica para verificar se existem erros
-    function verifyErrors() {
-        let foundError = false;
-
-        for(let error in field.validity) {
-            // se não for customError
-            // então verifica se tem erro
-            if (field.validity[error] && !field.validity.valid ) {
-                foundError = error
-            }
-        }
-        return foundError;
-    }
-
-    function customMessage(typeError) {
-        const messages = {
-            text: {
-                valueMissing: "Por favor, preencha este campo"
-            },
-            email: {
-                valueMissing: "Email é obrigatório",
-                typeMismatch: "Por favor, preencha um email válido"
-            }
-        }
-
-        return messages[field.type][typeError]
-    }
-
-    function setCustomMessage(message) {
-        const spanError = field.parentNode.querySelector("span.error")
-        
-        if (message) {
-            spanError.classList.add("active")
-            spanError.innerHTML = message
-        } else {
-            spanError.classList.remove("active")
-            spanError.innerHTML = ""
-        }
-    }
-
-    return function() {
-
-        const error = verifyErrors()
-
-        if(error) {
-            const message = customMessage(error)
-
-            field.style.borderColor = "red"
-            setCustomMessage(message)
-        } else {
-            field.style.borderColor = "green"
-            setCustomMessage()
-        }
-    }
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
 }
 
-
-function customValidation(event) {
-
-    const field = event.target
-    const validation = ValidateField(field)
-
-    validation()
-
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
 }
 
-for( field of fields ){
-    field.addEventListener("invalid", event => { 
-        // eliminar o bubble
-        event.preventDefault()
-
-        customValidation(event)
-    })
-    field.addEventListener("blur", customValidation)
-}
-
-
-document.querySelector("form")
-.addEventListener("submit", event => {
-    console.log("enviar o formulário")
-
-    // não vai enviar o formulário
-    event.preventDefault()
-})
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
